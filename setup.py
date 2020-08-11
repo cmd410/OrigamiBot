daef5359316b83c1606469a7e514af896ba56c85
Crystal Melting Dot <stresspassing@gmail.com>
2020-08-11 15:42:31 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requires = ['requests']
 
 setuptools.setup(
     name='origamibot',
-    version='1.2.1',
+    version='1.2.2',
     author='Crystal Melting Dot',
     author_email='stresspassing@gmail.com',
     description='Library for creating bots for telegram with Python.',
