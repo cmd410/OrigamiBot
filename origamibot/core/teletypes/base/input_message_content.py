52833b8747a87accf85dd0670353c65fb95831a0
Crystal Melting Dot <stresspassing@gmail.com>
2020-08-14 15:53:37 +0300

    Fix recursion error in InputMessageContent.__new__ method

--- origamibot/core/teletypes/base/input_message_content.py
+++ origamibot/core/teletypes/base/input_message_content.py
@@ -4,12 +4,12 @@ from .telegram_structure import TelegramStructure
 class InputMessageContent(TelegramStructure):
     def __new__(cls, **kwargs):
         if cls != InputMessageContent:
-            return cls(**kwargs)
+            return super(InputMessageContent, cls).__new__(cls)
+        keys = set(kwargs.keys())
         for i in InputMessageContent.__subclasses__():
-            try:
-                return i(**kwargs)
-            except TypeError:
-                pass
+            if not keys.issubset(i.fields_names()):
+                continue
+            return super(InputMessageContent, i).__new__(i)
         raise TypeError(
             f'Could not determine InputMessageContent from {kwargs}'
             )
