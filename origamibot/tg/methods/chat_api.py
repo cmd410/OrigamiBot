193ead09054c02d7cd4457dd2927b7c8804158be
Crystal Melting Dot <stresspassing@gmail.com>
2021-01-15 20:18:58 +0300

    some message edit methods

--- origamibot/tg/methods/chat_api.py
+++ origamibot/tg/methods/chat_api.py
@@ -1,9 +1,12 @@
-from typing import Optional, List
+from typing import Optional, List, Union
 
 from .api_base import APIBase
 from .util import own_result
 from ..common_typings import ChatID
-from ..types import Message, MessageEntity, InlineKeyboardMarkup
+from ..types import (Message,
+                     MessageEntity,
+                     InputMedia,
+                     InlineKeyboardMarkup)
 
 
 class ChatAPI(APIBase):
@@ -38,9 +41,65 @@ class ChatAPI(APIBase):
                           entities: Optional[List[MessageEntity]] = None,
                           disable_web_page_preview: Optional[bool] = None,
                           reply_markup: Optional[InlineKeyboardMarkup] = None,
-                          ) -> Message:
+                          ) -> Union[Message, bool]:
         """Use this method to edit text and game messages.
         On success, if the edited message is not an inline message,
         the edited Message is returned, otherwise True is returned.
         """
-        return Message.from_dict(self._simple_request("editMessageText", locals()))
+        assert (chat_id and message_id) or inline_message_id
+        result = self._simple_request("editMessageText", locals())
+        if isinstance(result, dict):
+            return Message.from_dict(result)
+        else:
+            return result
+
+    def edit_message_caption(self,
+                             chat_id: Optional[ChatID] = None,
+                             message_id: Optional[int] = None,
+                             inline_message_id: Optional[str] = None,
+                             caption: Optional[str] = None,
+                             parse_mode: Optional[str] = None,
+                             caption_entities: Optional[List[MessageEntity]] = None,
+                             reply_markup: Optional[InlineKeyboardMarkup] = None,
+                             ) -> Union[Message, bool]:
+        """Use this method to edit captions of messages.
+        On success, if the edited message is not an inline message,
+        the edited Message is returned, otherwise True is returned.
+        """
+        assert (chat_id and message_id) or inline_message_id
+        result = self._simple_request("editMessageCaption", locals())
+        if isinstance(result, dict):
+            return Message.from_dict(result)
+        else:
+            return result
+
+    def edit_message_media(self,
+                           media: InputMedia,
+                           chat_id: Optional[ChatID] = None,
+                           message_id: Optional[int] = None,
+                           inline_message_id: Optional[str] = None,
+                           reply_markup: Optional[InlineKeyboardMarkup] = None
+                           ):
+        """Use this method to edit animation,
+        audio, document, photo, or video messages.
+
+        If a message is part of a message album,
+        then it can be edited only to an audio
+        for audio albums, only to a document
+        for documentalbums and to a photo or a video
+        otherwise. When an inline message is edited,
+        a new file can't be uploaded.
+        Use a previously uploaded file via its
+        file_id or specify a URL.
+
+        On success,
+        if the edited message was sent by the bot,
+        the edited Message is returned,
+        otherwise True is returned.
+        """
+        assert (chat_id and message_id) or inline_message_id
+        result = self._simple_request("editMessageMedia", locals())
+        if isinstance(result, dict):
+            return Message.from_dict(result)
+        else:
+            return result
--- origamibot/tg/types/__init__.py
+++ origamibot/tg/types/__init__.py
@@ -8,4 +8,15 @@ from .chat_photo import ChatPhoto
 from .photo_size import PhotoSize
 from .animation import Animation
 from .audio import Audio
-from .inline_keyboard import InlineKeyboardButton, InlineKeyboardMarkup, LoginUrl
+
+from .inline_keyboard import (InlineKeyboardButton,
+                              InlineKeyboardMarkup,
+                              LoginUrl)
+
+from .input_media import (InputFile,
+                          InputMedia,
+                          InputMediaPhoto,
+                          InputMediaVideo,
+                          InputMediaAnimation,
+                          InputMediaAudio,
+                          InputMediaDocument)
