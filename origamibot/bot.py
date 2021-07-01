b7fc0ad54df634877a722c1fd03188a5e12332be
Crystal Melting Dot <stresspassing@gmail.com>
2021-07-01 21:07:13 +0300

    Starting Bot class

---
+++ origamibot/bot.py
@@ -0,0 +1,25 @@
+from typing import List, Optional
+
+from httpx._types import ProxiesTypes
+
+from ._hints import URLTypes, UpdateTypeStr
+from .api import UpdateAPI
+
+
+
+class Bot(UpdateAPI):
+    def __init__(self,
+                 token: str,
+                 *,
+                 host: URLTypes = 'https://api.telegram.org',
+                 proxies: ProxiesTypes = None,
+                 update_limit: Optional[int] = None,
+                 update_timeout: Optional[int] = None,
+                 allowed_updates: Optional[List[UpdateTypeStr]] = None,
+                 ) -> None:
+
+        params = locals().copy()
+        del params['self']
+        del params['token']
+
+        super().__init__(token, **params)
