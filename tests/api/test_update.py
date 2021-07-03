e694bac58c7737ed9763d6a63b48356fdf5b1bef
Crystal Melting Dot <stresspassing@gmail.com>
2021-07-03 20:40:34 +0300

    rename update_api to api in tests

--- tests/api/test_update.py
+++ tests/api/test_update.py
@@ -7,30 +7,30 @@ from origamibot.types import Update
 from .. import token, webhook
 
 
-@pytest.fixture
-def update_api(token):
+@pytest.fixture(scope="module")
+def api(token):
     return UpdateAPI(token=token)
 
 
 @pytest.mark.asyncio
-async def test_update_api(update_api: UpdateAPI):
+async def test_update_api(api: UpdateAPI):
     """Test UpdateAPI class
     """
-    updates = await update_api.get_updates(timeout=0)
+    updates = await api.get_updates(timeout=0)
     assert isinstance(updates, list)
     assert len(updates) == 0 or all(isinstance(i, Update) for i in updates)
 
 
 
 @pytest.mark.asyncio
-async def test_webhooks(update_api: UpdateAPI, webhook: str):
-    whi = await update_api.get_webhook_info()
+async def test_webhooks(api: UpdateAPI, webhook: str):
+    whi = await api.get_webhook_info()
     if whi.url:
-        assert await update_api.delete_webhook()
+        assert await api.delete_webhook()
     
-    assert await update_api.set_webhook(url=webhook)
+    assert await api.set_webhook(url=webhook)
     
-    whi = await update_api.get_webhook_info()
+    whi = await api.get_webhook_info()
     
     assert whi.url == webhook
-    assert await update_api.delete_webhook()
+    assert await api.delete_webhook()
