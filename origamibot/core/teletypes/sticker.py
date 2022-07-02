f8c2ac3aaa0f14c9633b55ec281a93d8df3e4426
Crystal Melting Dot <stresspassing@gmail.com>
2022-07-03 01:08:53 +0300

    add `premium_animation` to sticker

--- origamibot/core/teletypes/sticker.py
+++ origamibot/core/teletypes/sticker.py
@@ -1,6 +1,9 @@
+from typing import Optional
+
 from .base import TelegramStructure, Field
 from .photo_size import PhotoSize
 from .mask_position import MaskPosition
+from .file import File
 
 
 class Sticker(TelegramStructure):
@@ -19,7 +22,7 @@ class Sticker(TelegramStructure):
     mask_position = Field()
     file_size = Field()
     is_video = Field()
-
+    premium_animation = Field()
 
     def __init__(self,
                  file_id: str,
@@ -28,11 +31,12 @@ class Sticker(TelegramStructure):
                  height: int,
                  is_animated: bool,
                  is_video: bool,
-                 thumb: PhotoSize = None,
-                 emoji: str = None,
-                 set_name: str = None,
-                 mask_position: MaskPosition = None,
-                 file_size: int = None,
+                 thumb: Optional[PhotoSize] = None,
+                 emoji: Optional[str] = None,
+                 set_name: Optional[str] = None,
+                 mask_position: Optional[MaskPosition] = None,
+                 file_size: Optional[int] = None,
+                 premium_animation: Optional[File] = None
                  ):
         self.file_id = \
             Field(file_id, [str])
@@ -66,3 +70,6 @@ class Sticker(TelegramStructure):
 
         self.file_size = \
             Field(file_size, [int])
+        
+        self.premium_animation = \
+            Field(premium_animation, [File])
