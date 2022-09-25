ed7267d0e6f44d316373afe765e4f0a66d441a1d
Crystal Melting Dot <stresspassing@gmail.com>
2022-09-25 14:35:48 +0300

    telegram-text test coverage

--- tests/test_send_message.py
+++ tests/test_send_message.py
@@ -1,6 +1,18 @@
+from pytest import skip
 from . import bot, token, private_cid
+
+try:
+  from telegram_text import Bold
+  tg_text_present = True
+except ImportError:
+  tg_text_present = False
 
 
 def test_send_message(bot, private_cid):
     message = bot.send_message(chat_id=private_cid, text='Test message')
     assert message
+
+
+def test_telegram_text(bot, private_cid):
+  if not tg_text_present: skip("telegram_text is not installed")
+  message = bot.send_message(chat_id=private_cid, text=Bold("Bold text message"))
